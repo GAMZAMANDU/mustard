3b58dff58faa67f4b31c4e9a5a4339903ce928f9
galbimandu <galbimandu1126@gmail.com>
2025-05-07 04:54:36 +0900

    chore : 완성되지 않는 버튼 비활성화

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
       <Panel borderDirection="bottom" className="min-h-[2rem] p-[1rem] flex items-center justify-between">
         <img src="/assets/Logo.svg" alt="Logo" className="h-[1.5rem]"/>
         <div className="flex gap-[1.5rem]">
-          <Button text="New Project" />
+          {/* <Button text="New Project" /> */}
           <img src="/assets/Avatar.svg" alt="Avatar" className="h-[2.25rem]" />
         </div>
       </Panel>
