ae647751d4ab73e29582302dde6f0f20238726f1
galbimandu <galbimandu1126@gmail.com>
2025-05-06 21:24:26 +0900

    add : header 제작

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import Button from "./Button";
+import Panel from "./layout/Panel";
+
+const Header = () => {
+  return (
+    <header className="h-full p-8">
+      <Panel borderDirection="bottom" className="min-h-[2rem] p-[1rem] flex items-center justify-between">
+        <img src="/assets/Logo.svg" alt="Logo" className="h-[1.5rem]"/>
+        <div className="flex gap-[1.5rem]">
+          <Button text="New Project" />
+          <img src="/assets/Avatar.svg" alt="Avatar" className="h-[2.25rem]" />
+        </div>
+      </Panel>
+    </header>
+  );
+};
+
+export default Header;
