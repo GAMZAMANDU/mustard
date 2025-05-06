f43f7e141dddaab9ab8c5e1de0bffb1d6674386e
galbimandu <galbimandu1126@gmail.com>
2025-05-06 23:07:59 +0900

    add : App에 ReviewAside 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Header from "./components/Header";
+import ReviewAside from "./components/ReviewAside";
 
 function App() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Header />
+      <main className="flex flex-1">
+        <ReviewAside />
+      </main>
     </div>
   );
 }
