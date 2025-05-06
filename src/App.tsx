992c5734a7c3766a1b4c4cf1c07ac86c1815a4dc
galbimandu <galbimandu1126@gmail.com>
2025-05-07 01:52:09 +0900

    add : useEffect로 목데이터 전역변수에 삽입

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import Header from "./components/Header";
 import ReviewAside from "./components/ReviewAside";
+import { useEffect } from "react";
+import { generateMockData } from "./data/generateMockData";
+import useStore from "./store/useReviewPage";
 
 function App() {
+  const setPages = useStore((state) => state.setPages);
+  const setReviewers = useStore((state) => state.setReviewers);
+
+  useEffect(() => {
+    const { pages, reviewers } = generateMockData(3, 4, 5);
+    setPages(pages);
+    setReviewers(reviewers);
+  }, []);
+  
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
