54f14609cd1c35960da68d4f6c32fcd1507e1f73
galbimandu <galbimandu1126@gmail.com>
2025-05-07 00:49:49 +0900

    add : ReviewAsideHeader에 Store 적용

--- src/components/ReviewAside.tsx
+++ src/components/ReviewAside.tsx
@@ -11,7 +11,6 @@ const ReviewAside = () => {
         <ReviewAsidePages />
         <ReviewAsideReviewersList />
       </Panel>
-
     </aside>
   );
 };
--- src/components/ReviewAsideHeader.tsx
+++ src/components/ReviewAsideHeader.tsx
@@ -1,26 +1,37 @@
-import React from 'react'
 import Text from "./Text";
+import useStore from "../store/useReviewPage";
 
 const ReviewAsideHeader = () => {
+  const reviewers = useStore((state) => state.reviewers);
+  const reviewersLength = reviewers.length.toString();
+  const comments = useStore((state) => state.comments);
+  const commentsLength = comments.length.toString();
+
   return (
     <header>
-    <Text color="white" weight="bold" size="lg" text="E-commerce Redesign" className="!mb-[1rem]"/>
-    <div className="flex gap-[1.5rem]">
-      <div>
-        <Text color="white" weight="bold" size="lg" text="16"/>
-        <Text color="gray" weight="normal" size="sm" text="Comments" />
+      <Text
+        color="white"
+        weight="bold"
+        size="lg"
+        text="E-commerce Redesign"
+        className="!mb-[1rem]"
+      />
+      <div className="flex gap-[1.5rem]">
+        <div>
+          <Text color="white" weight="bold" size="lg" text={commentsLength} />
+          <Text color="gray" weight="normal" size="sm" text="Comments" />
+        </div>
+        <div>
+          <Text color="white" weight="bold" size="lg" text={reviewersLength} />
+          <Text color="gray" weight="normal" size="sm" text="Reviewers" />
+        </div>
+        <div>
+          <Text color="white" weight="bold" size="lg" text="16" />
+          <Text color="gray" weight="normal" size="sm" text="Likes" />
+        </div>
       </div>
-      <div>
-        <Text color="white" weight="bold" size="lg" text="16"/>
-        <Text color="gray" weight="normal" size="sm" text="Reviewers" />
-      </div>
-      <div>
-        <Text color="white" weight="bold" size="lg" text="16"/>
-        <Text color="gray" weight="normal" size="sm" text="Likes" />
-      </div>
-    </div>
-  </header>
-  )
-}
+    </header>
+  );
+};
 
-export default ReviewAsideHeader
+export default ReviewAsideHeader;
