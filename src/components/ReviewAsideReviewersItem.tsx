7929e2f08bfd440824a893e4ce4ecb2206dd7300
galbimandu <galbimandu1126@gmail.com>
2025-05-07 02:26:01 +0900

    add : ReviewAsideReviewers에 데이터 바인딩, 선택 시 효과도 적용

--- src/components/ReviewAsideReviewersItem.tsx
+++ src/components/ReviewAsideReviewersItem.tsx
@@ -1,22 +1,40 @@
 import Text from "./Text";
+import useStore from "../store/useReviewPage";
 
-interface ReviewAsidePagesBlockProps {
-  who: string;
+interface ReviewAsideReviewersItemProps {
+  text: string;
   selected?: boolean;
+  id: string;
 }
 
-const ReviewAsidePagesBlock = ({
-  who: text,
+const ReviewAsideReviewersItem = ({
+  text,
   selected = false,
-}: ReviewAsidePagesBlockProps) => {
+  id,
+}: ReviewAsideReviewersItemProps) => {
+  const setReviewerId = useStore((state) => state.setReviewerId);
   return (
     <div
-      className="flex gap-[0.5rem] items-center mt-[0.75rem]"
+      className="gap-[0.5rem] cursor-pointer flex h-[36px] px-[12px] items-center self-stretch rounded-[4px] mt-[0.75rem]"
+      onClick={() => setReviewerId(id)}
     >
-      <img src="/assets/Avatar.svg" alt="avatar" className="w-[24px] h-[24px]"/>
-      <Text color="white" weight="normal" size="md" text={text} />
+      <img
+        src="/assets/Avatar.svg"
+        alt="avatar"
+        className={`bg-[#1A1A1A] rounded-[14px] w-[24px] h-[24px] ${
+          selected &&
+          "outline outline-2 outline-[#3ECF8E] outline-offset-[-2px]"
+        }`}
+      />
+      <Text
+        color="white"
+        weight="normal"
+        size="md"
+        text={text}
+        className="cursor-pointer"
+      />
     </div>
   );
 };
 
-export default ReviewAsidePagesBlock;
+export default ReviewAsideReviewersItem;
--- src/components/ReviewAsideReviewersList.tsx
+++ src/components/ReviewAsideReviewersList.tsx
@@ -1,15 +1,26 @@
 import Text from "./Text";
-import ReviewAsideReviewersItem from './ReviewAsideReviewersItem';
+import ReviewAsideReviewersItem from "./ReviewAsideReviewersItem";
+import useStore from "../store/useReviewPage";
 
 const ReviewAsideReviewersList = () => {
+  const reviewers = useStore((state) => state.reviewers);
+  const reviewer_id = useStore((state) => state.reviewer_id);
+
   return (
     <section>
       <Text color="gray" weight="bold" size="md" text="REVIEWERS" />
       <div className="flex flex-col">
-        <ReviewAsideReviewersItem who="John Doe" />
-        <ReviewAsideReviewersItem who="John Doe" />
+        {reviewers.map((reviewer) => (
+          <ReviewAsideReviewersItem
+            key={reviewer.id}
+            text={reviewer.name}
+            id={reviewer.id}
+            selected={reviewer.id === reviewer_id}
+          />
+        ))}
       </div>
-    </section>  )
-}
+    </section>
+  );
+};
 
-export default ReviewAsideReviewersList
+export default ReviewAsideReviewersList;
