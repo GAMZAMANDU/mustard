0908aa255feb409c38354c44a5cc65133767bdfa
galbimandu <galbimandu1126@gmail.com>
2025-05-06 23:04:38 +0900

    add : ReviewAsidePages 제작

---
+++ src/components/ReviewAsidePages.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Text from "./Text";
+import ReviewAsidePagesBlock from "./ReviewAsidePagesBlock";
+
+const ReviewAsidePages = () => {
+  return (
+    <section>
+      <Text color="gray" weight="bold" size="md" text="PAGES" />
+      <div className="flex flex-col">
+        <ReviewAsidePagesBlock text="Home" selected />
+        <ReviewAsidePagesBlock text="Products" />
+      </div>
+    </section>
+  );
+};
+
+export default ReviewAsidePages;
