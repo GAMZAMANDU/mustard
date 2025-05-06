126b3608c592aac38d1049cd7d83c70af6fd9a8a
galbimandu <galbimandu1126@gmail.com>
2025-05-07 02:07:33 +0900

    add : ReviewAsidePage에 데이터 바인딩

--- src/components/ReviewAsidePages.tsx
+++ src/components/ReviewAsidePages.tsx
@@ -1,13 +1,18 @@
 import Text from "./Text";
 import ReviewAsidePagesBlock from "./ReviewAsidePagesBlock";
+import useStore from "../store/useReviewPage";
 
 const ReviewAsidePages = () => {
+  const pages = useStore((state) => state.pages);
+  const page_id = useStore((state) => state.page_id);
+  
   return (
     <section>
       <Text color="gray" weight="bold" size="md" text="PAGES" />
       <div className="flex flex-col">
-        <ReviewAsidePagesBlock text="Home" selected />
-        <ReviewAsidePagesBlock text="Products" />
+        {pages.map((page) => (
+          <ReviewAsidePagesBlock key={page.id} text={page.name} selected={page.id === page_id} id={page.id} />
+        ))}
       </div>
     </section>
   );
--- src/components/ReviewAsidePagesBlock.tsx
+++ src/components/ReviewAsidePagesBlock.tsx
@@ -1,21 +1,32 @@
 import Text from "./Text";
+import useStore from "../store/useReviewPage";
 
 interface ReviewAsidePagesBlockProps {
   text: string;
   selected?: boolean;
+  id: string;
 }
 
 const ReviewAsidePagesBlock = ({
   text,
   selected = false,
+  id,
 }: ReviewAsidePagesBlockProps) => {
+  const setPageId = useStore((state) => state.setPageId);
+
   return (
     <div
-      className={`flex h-[36px] px-[12px] items-center self-stretch rounded-[4px] mt-[0.75rem] ${
+      className={`cursor-pointer flex h-[36px] px-[12px] items-center self-stretch rounded-[4px] mt-[0.75rem] ${
         selected ? "bg-[#1A1A1A]" : ""
       }`}
+      onClick={() => setPageId(id)}
     >
-      <Text color={selected ? "primary" : "gray"} weight="normal" size="md" text={text} />
+      <Text
+        color={selected ? "primary" : "gray"}
+        weight="normal"
+        size="md"
+        text={text}
+      />
     </div>
   );
 };
