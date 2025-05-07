60dec18e0e6e15c3c11831390d18098908b5229d
galbimandu <galbimandu1126@gmail.com>
2025-05-07 09:18:41 +0900

    add : LOGO 클릭 시 홈으로 가게함

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
-import Button from "./Button";
 import Panel from "./layout/Panel";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <header className="h-full p-8">
       <Panel borderDirection="bottom" className="min-h-[2rem] p-[1rem] flex items-center justify-between">
-        <img src="/assets/Logo.svg" alt="Logo" className="h-[1.5rem]"/>
+        <img src="/assets/Logo.svg" alt="Logo" className="h-[1.5rem] select-none cursor-pointer" onClick={() => navigate("/")}/>
         <div className="flex gap-[1.5rem]">
           {/* <Button text="New Project" /> */}
           <img src="/assets/Avatar.svg" alt="Avatar" className="h-[2.25rem]" />
